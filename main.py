9ac974f47d50e7a95de260a3d997f476c7bfd3e0
Eugenio Dacrema <151853536+eudacrema@users.noreply.github.com>
2023-12-07 14:21:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
-
-
+import argparse
 from My_functions import compare_trends
 
+def main(country_name, end_year):
+    results = compare_trends(country_name, end_year)
+    print(results)
 
-country_name = "Angola"
-end_year = 2023
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Process some inputs.')
+    parser.add_argument('country_name', type=str, help='Name of the country')
+    parser.add_argument('end_year', type=int, help='End year')
+
+    args = parser.parse_args()
+
+    main(args.country_name, args.end_year)
+
+
 
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    country_name = input("Enter the name of the country: ")
-    end_year = int(input("Enter the year: "))
+#if __name__ == '__main__':
+    #country_name = input("Enter the name of the country: ")
+    #end_year = int(input("Enter the year: "))
 
-    compare_trends(country_name, end_year)
+    #compare_trends(country_name, end_year)
 
 
 
